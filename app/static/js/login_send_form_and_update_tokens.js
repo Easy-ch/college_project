90ae649e0565b71dc6013c8b0bbd72e7d4dd7cbc
Easy-ch <136355212+Easy-ch@users.noreply.github.com>
2024-12-09 20:32:15 +0300

    checkbox_password not finished

--- app/static/js/login_send_form_and_update_tokens.js
+++ app/static/js/login_send_form_and_update_tokens.js
@@ -117,3 +117,21 @@ async function fetchProtectedRoute() {
 
 fetchProtectedRoute();
 
+async function check_password() {
+    if (document.getElementById('show_password').checked){
+        document.getElementById('password').setAttribute('type', 'text');
+    } else {
+        document.getElementById('password').setAttribute('type', 'password');
+    }
+}
+
+
+async function check_password_confirm() {
+    if (document.getElementById('show_password_confirm').checked){
+        document.getElementById('password_confirm').setAttribute('type', 'text');
+    } else {
+        document.getElementById('password_confirm').setAttribute('type', 'password');
+    }
+}
+
+
